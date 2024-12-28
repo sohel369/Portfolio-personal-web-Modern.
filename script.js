eff42a7db3599257e8c2c294ac2edf4bc4ad010b
sohel rana <sohel0130844@gmail.com>
2024-12-29 00:08:40 +0600

    first upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const mobile_device = document.querySelector('.mobile_device');
+mobile_device.addEventListener('click', () => {
+    document.querySelector('.main_menu').classList.toggle('open');
+})
