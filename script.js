3a3feaf5cb370de979595430ce040a90be403b3e
sohel rana <sohel0130844@gmail.com>
2025-01-03 23:53:52 +0600

    projects section finish

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const mobile_device = document.querySelector('.mobile_device');
-mobile_device.addEventListener('click', () => {
-    document.querySelector('.main_menu').classList.toggle('open');
-})
+const main_menu = document.querySelector('.main_menu');
+
+// Toggle the menu when clicking on the mobile device toggle
+mobile_device.addEventListener('click', (event) => {
+    event.stopPropagation(); // Prevent the click from propagating to the window
+    main_menu.classList.toggle('open');
+});
+
+// Close the menu on scroll
+window.addEventListener('scroll', () => {
+    main_menu.classList.remove('open');
+});
+
+// Close the menu when clicking outside
+window.addEventListener('click', function(event) {
+    // Ensure the click is not inside the menu or on the toggle button
+    if (!main_menu.contains(event.target) && event.target !== mobile_device) {
+        main_menu.classList.remove('open');
+    }
+});
